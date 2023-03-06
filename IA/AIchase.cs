0f54dce6950dd70f4f9c2b96b1f321bf8d3e8c02
Yuzunan <lena.cerinotti@epita.fr>
2023-03-06 22:08:55 +0100

    mini association entre push

--- IA/AIchase.cs
+++ IA/AIchase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AIchase : MonoBehaviour
@@ -58,11 +59,22 @@ public class AIchase : MonoBehaviour
                 {
                     speed += spurt;
                     spurt = 0;
+                    if (distance < 1.5)
+                    {
+                        player.gameObject.GetComponent<PlayerHealth>().UpdateHealth(1);
+                        activate = false;
+                    }
                 }
             }
-
-
-
+        }
+        else
+        {
+            IEnumerator waiter()
+            {
+                yield return new WaitForSecondsRealtime(2);
+                this.GameObject().SetActive(false);
+            }
+            StartCoroutine(waiter());
         }
     }
 }

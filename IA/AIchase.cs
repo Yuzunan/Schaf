2f9e2051797e60a93d60fb43b4c8b902df09e5d2
Théo Courson <theo.courson@epita.fr>
2023-03-06 21:32:52 +0100

    J-3

--- IA/AIchase.cs
+++ IA/AIchase.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class AIchase : MonoBehaviour
 {
     public Transform player;
+    public SpriteRenderer spriteRenderer;
+    public Sprite UpSprite;
+    public Sprite LeftSprite;
+    public Sprite RightSprite;
+    public Sprite DownSprite;
     private float distance;
     public float speed;
     public float spurt;
@@ -23,8 +27,29 @@ public class AIchase : MonoBehaviour
         if (activate)
         {
             distance = Vector2.Distance(player.position, transform.position);
-
             Vector2 direction = player.transform.position - transform.position;
+            if (direction.x > direction.y)
+            {
+                if (direction.x > -1 * direction.y)
+                {
+                    spriteRenderer.sprite = RightSprite; 
+                }
+                else
+                {
+                    spriteRenderer.sprite = DownSprite; 
+                }
+            }
+            else
+            {
+                if (-1 * direction.x > direction.y)
+                {
+                    spriteRenderer.sprite = LeftSprite; 
+                }
+                else
+                {
+                    spriteRenderer.sprite = UpSprite; 
+                }
+            }
             transform.position =
                 Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             if (spurt >= 0)
@@ -33,26 +58,11 @@ public class AIchase : MonoBehaviour
                 {
                     speed += spurt;
                     spurt = 0;
-                    if (distance < 1.5)
-                    {
-                        player.gameObject.GetComponent<PlayerHealth>().UpdateHealth(1);
-                        activate = false;
-                    }
-                    
                 }
             }
-        }
-        else
-        {
-            IEnumerator waiter()
-            {
-                yield return new WaitForSecondsRealtime(2);
-                this.GameObject().SetActive(false);
-            }
-            StartCoroutine(waiter());
-            
+
+
+
         }
     }
-    
-
 }
